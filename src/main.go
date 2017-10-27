126f10aad88f8fd0a5c699cbd87a94e2877037a2
Riccardo Casatta <riccardo@casatta.it>
2017-10-27 12:27:13 +0200

    adding java version

--- src/main.go
+++ src/main.go
@@ -13,11 +13,11 @@ func main() {
 	rand.Read(current)
 	start := time.Now()
 
-	for i := int64(0) ; i < 1000000000; i++ {
-		if(i%1000000==0) {
+	for i := int64(0) ; i < 10000000000; i++ {
+		if(i%10000000==0) {
 			elapsed := time.Since(start).Nanoseconds()/1000
 
-			fmt.Printf("%d %x %f\n", i, current, float64(i)/float64(elapsed))
+			fmt.Printf("%d %x %f Mhash/s\n", i, current, float64(i)/float64(elapsed))
 		}
 		result := sha256.Sum256([]byte(current))
 		current = result[:];
